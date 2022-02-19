1696fad12288443ada75fe001c87abfffe432fde
Randy Price <edward.randolph.price@gmail.com>
2022-02-19 17:00:56 -0500

    Changed let () statment in toplevel.ml to accept an input .pr file for scanning/parsing/tree generation.

--- scanner-parser/ast.ml
+++ scanner-parser/ast.ml
@@ -1,1 +1,4 @@
 type program = string list
+
+let string_of_program tkns =
+  String.concat "\n" tkns
--- scanner-parser/toplevel.ml
+++ scanner-parser/toplevel.ml
@@ -1,19 +1,17 @@
 open Ast
-
-(* let run tkn_list = function
-    Prog(p) -> p :: tkn_list *)
-
-(* let rec print_tkn_list = function
+let rec run = function
     [] -> ()
-  | t::ts -> print_endline(t); print_tkn_list ts *)
-
-  let rec run = function
-      [] -> ()
-    | t::ts -> print_endline t; run ts
+  | t::ts -> print_endline t; run ts
 
 let () =
+  let usage_msg = "usage: ./toplevel.native [file.pr]" in
+  let channel = ref stdin in
+  Arg.parse [] (fun file -> channel:= open_in file) usage_msg;
+  let lexbuf = Lexing.from_channel !channel in
+  let ast = Parser.program Scanner.token lexbuf in
+  print_string (Ast.string_of_program ast)
+  
+(* let () =
   let lex_buf = Lexing.from_channel stdin in
   let prg = Parser.program Scanner.token lex_buf in
-  run prg
-  (* let result = run [] prg in *)
-  (* print_tkn_list result *)
+  run prg *)
