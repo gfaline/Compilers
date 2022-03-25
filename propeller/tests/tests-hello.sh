e8a22f3d9af0f9b9ab35e887de1958b13a2d4243
Chris Xiong <chirs241097@gmail.com>
2022-03-25 00:26:07 -0400

    Add a failure case for hello world tests.

--- propeller/tests/tests-hello.sh
+++ propeller/tests/tests-hello.sh
@@ -3,7 +3,8 @@ source $(dirname "$0")/testutil.sh
 cd $(dirname "$0")/hello
 
 RET=0
-CompiledCheckExpect hello.pr hello.exp || (echo "hello.pr failed" && RET=1)
-CompiledCheckExpectWReturnCode hello_ret.pr hello.exp IGNORED 5 || (echo "hello_ret.pr failed" && RET=1)
-[ $RET -eq 0 ] && echo "both tests passed"
+CompiledCheckExpect hello.pr hello.exp || { echo "hello.pr failed"; RET=1; }
+CompiledCheckExpectWReturnCode hello_ret.pr hello.exp IGNORED 5 || { echo "hello_ret.pr failed"; RET=1; }
+CheckFail "$TheWrapper hello_bad.pr" IGNORED || { echo "hello_bad.pr failed"; RET=1; }
+[ $RET -eq 0 ] && echo "all 3 tests passed"
 exit $RET
