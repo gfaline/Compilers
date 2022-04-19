6fc874ef58245e7519c18ad80ea5b395476245c9
Randy Price <edward.randolph.price@gmail.com>
2022-04-19 10:02:53 -0400

    Add global variables to codegen.ml

--- propeller/codegen.ml
+++ propeller/codegen.ml
@@ -4,23 +4,32 @@ open Sast
 
 module StringMap = Map.Make(String)
 
-let translate ( (*globals, objects*) _, _, functions) =
+let translate (globals, _ (* objects *), functions) =
   let context = L.global_context () in
 
   let i32_t      = L.i32_type      context
   and i8_t       = L.i8_type       context
   and i1_t       = L.i1_type       context
   and float_t    = L.double_type   context
+  and void_t     = L.void_type     context
   and the_module = L.create_module context "Propeller" in
 
   let ltype_of_typ = function
       A.Int   -> i32_t
     | A.Float -> float_t
     | A.Bool  -> i1_t
+    | A.Void  -> void_t
     | _       -> i32_t
   in
 
-  (*let global_vars : L.llvalue StringMap.t = StringMap.empty in*)
+  let global_vars : L.llvalue StringMap.t =
+    let global_var m (t, n) = 
+      let init = match t with
+          A.Float -> L.const_float (ltype_of_typ t) 0.0
+        | _ -> L.const_int (ltype_of_typ t) 0 in
+      StringMap.add n (L.define_global n init the_module) m
+    in
+    List.fold_left global_var StringMap.empty globals in
 
   let print_t : L.lltype = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let print_func : L.llvalue = L.declare_function "printf" print_t the_module in
